8d0fb40f886c310e278883c4c96e36b8a745c786
Maricrin Paradas <maaricrin@gmail.com>
2023-10-10 19:43:57 -0300

    proyecto proyecto

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 8001;
 const productsRoute = require("./src/routes/productRoutes");
 const cartRoute = require("./src/routes/cartRoutes");
 const notFoundRoute = require("./src/routes/main");
